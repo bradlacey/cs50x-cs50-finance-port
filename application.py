5b39cfe49c47b9a77393d8b9549647a19692c1f0
Brad Lacey <bradlacey@gmail.com>
2018-02-14 00:44:27 +1100

    bug-fixing + testing

--- application.py
+++ application.py
@@ -331,8 +331,9 @@ def login():
 
         # ensure username exists and password is correct
         # debugging
+
         # if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
-        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows["hash"]):
+        if len(rows) < 1 or not pwd_context.verify(request.form.get("password"), rows.hash):
             return apology("invalid username and/or password")
 
         # remember which user has logged in
