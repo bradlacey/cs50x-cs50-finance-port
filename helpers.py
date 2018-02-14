8f1b0c8f6b739482052156c2e6a79993e9a9e2f7
Brad Lacey <bradlacey@gmail.com>
2018-02-14 18:43:10 +1100

    apology fix & testing 05

--- helpers.py
+++ helpers.py
@@ -6,18 +6,7 @@ from functools import wraps
 
 # def apology(message, code=400):
 def apology(message):
-    """Renders message as an apology to user."""
-    # def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        # for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-        #                  ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-        #     s = s.replace(old, new)
-        # return s
-    # return render_template("apology.html", top=code, bottom=escape(message)), code
+    # image source: http://www.essex1.com/people/paul/bible56.html
     return render_template("apology.html", message = message)
 
 def login_required(f):
