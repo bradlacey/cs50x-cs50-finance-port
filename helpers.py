226a1c8a04e77eecc1c40b6b7105fbfc8ed1c706
Brad Lacey <bradlacey@gmail.com>
2018-02-14 18:32:28 +1100

    apology fix & testing 03

--- helpers.py
+++ helpers.py
@@ -5,20 +5,21 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 
-def apology(message, code=400):
+# def apology(message, code=400):
+def apology(message)
     """Renders message as an apology to user."""
-    def escape(s):
+    # def escape(s):
         """
         Escape special characters.
 
         https://github.com/jacebrowning/memegen#special-characters
         """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
-
+        # for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+        #                  ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+        #     s = s.replace(old, new)
+        # return s
+    # return render_template("apology.html", top=code, bottom=escape(message)), code
+    return render_template("apology.html", message = message)
 
 def login_required(f):
     """
