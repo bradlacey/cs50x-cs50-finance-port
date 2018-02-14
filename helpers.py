628b27e600b6473434a6a4e35ae67745595c5987
Brad Lacey <bradlacey@gmail.com>
2018-02-14 18:38:35 +1100

    apology fix & testing 04

--- helpers.py
+++ helpers.py
@@ -4,7 +4,6 @@ import urllib.request
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-
 # def apology(message, code=400):
 def apology(message):
     """Renders message as an apology to user."""
