e1263495a6cff058438d0b2abe0d1d8ce2d4d8df
Brad Lacey <bradlacey@gmail.com>
2018-02-16 02:23:36 +1100

    adding quandl api  call to helpers.py

--- helpers.py
+++ helpers.py
@@ -34,6 +34,7 @@ def lookup(symbol):
     if "," in symbol:
         return None
 
+"""
     # query Yahoo for quote
     # http://stackoverflow.com/a/21351911
     try:
@@ -65,6 +66,7 @@ def lookup(symbol):
 
     except:
         pass
+"""
 
     # query Alpha Vantage for quote instead
     # https://www.alphavantage.co/documentation/
@@ -97,7 +99,46 @@ def lookup(symbol):
         }
 
     except:
-        return None
+        pass
+        # return None
+
+    try:
+     	# query QuandL for quote
+    	# https://blog.quandl.com/api-for-stock-data
+        # try:
+        # remember that f-strings need Python 3.6
+        url = f"https://www.quandl.com/api/v3/datasets/EOD/"
+        # url = "https://www.quandl.com/api/v3/datasets/EOD/"
+        url += symbol
+        url += ".csv?api_key=Yp6bSmznThD2mfDnUFyQ&column_index=1&rows=1"
+        # full URL: https://www.quandl.com/api/v3/datasets/EOD/{{symbol}}.csv?api_key=Yp6bSmznThD2mfDnUFyQ&column_index=1&rows=1
+        # e.g.:     https://www.quandl.com/api/v3/datasets/EOD/AAPL.csv?api_key=Yp6bSmznThD2mfDnUFyQ&column_index=1&rows=1
+
+        request = "EOD/"
+        request += symbol
+
+        # shift 48 spaces to account for whitespace in data
+        data_start = 48
+
+        # ABOVE: TESTED
+
+        # see documentation:
+        # https://docs.quandl.com/docs/in-depth-usage
+
+        # HERE: NEW
+
+        # debugging:
+        # working
+        # return url
+
+        with urllib.request.urlopen(url) as csvfile:
+            results = csvfile.read()
+            return type(results)
+            for row in results:
+                data += str(row)
+        return data
+    except:
+        return apology("there was a fatal error so bad luck eh")
 
 
 def usd(value):
